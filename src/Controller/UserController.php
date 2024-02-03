59de1291349a5808b5e308612b11ad9d30ae9574
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-02-03 16:45:06 +0100

    feat: created user entity with respective migration file

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,16 +2,42 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     #[Route('/check')]
 
-    public function userList(): Response
+    public function userList( ): Response
     {
-       return new Response('Hola mundo');
+ 
+        $response = new JsonResponse();
+        $this->logger->info('list action called');
+        $response->setData([
+            'success' => true,
+            'data' =>
+            [
+                'id' => 1,
+                'username' => "admin",
+                'email' => "admin@test.com"
+            ],
+            [
+                'id' => 2,
+                'username' => "user",
+                'email' => "user@test.com"
+            ]
+        ]);
+        return $response;
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[HasLifecycleCallbacks]
+
+
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created_at = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updated_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+     /**
+     * @ORM\PrePersist
+     */
+    public function prePersist(): void
+    {
+        $this->created_at = $this->created_at ?? new \DateTime();
+        $this->updated_at = $this->updated_at ?? new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function preUpdate(): void
+    {
+        $this->updated_at = new \DateTime();
+    }
+
+}
